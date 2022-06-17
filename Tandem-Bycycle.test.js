810e4fa36a835a729162a569c4feacde18ed7951
RMo <morici23@gmail.com>
2022-06-16 22:15:10 -0400

    Start print-elevation problem. Write out thoughts.

---
+++ Tandem-Bycycle.test.js
@@ -0,0 +1,55 @@
+function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
+
+    redShirtSpeeds.sort((a,b) => a - b)
+    blueShirtSpeeds.sort((a,b) => a - b)
+
+    let totalSpeed = 0
+
+    if(!fastest) redShirtSpeeds.reverse()
+
+    for(let i = 0; i < redShirtSpeeds.length; i++) {
+        if(fastest) {
+            totalSpeed += Math.max(redShirtSpeeds[i], blueShirtSpeeds[i])
+        } else {
+            totalSpeed += Math.min(redShirtSpeeds[i], blueShirtSpeeds[i]) 
+        }
+    }
+
+    return totalSpeed;
+}
+  
+describe('Tandem-Bycycle', ()=> {
+
+    
+    test('if input arrays are the same length', ()=> {
+        let simpleRed = [2]
+        let simpleBlue = [1]
+        expect((simpleRed.length === simpleBlue.length)).toBeTruthy()
+    })
+
+    it('should return 2', ()=> {
+        let simpleRed = [2]
+        let simpleBlue = [1]
+        expect(tandemBicycle(simpleRed, simpleBlue, true)).toBe(2)
+    })
+    // max
+    it('should return 1', ()=> {
+        let simpleRed = [2]
+        let simpleBlue = [1]
+        expect(tandemBicycle(simpleRed, simpleBlue, false)).toBe(1)
+    })
+    
+    it('should return 9', ()=> {
+        let redShirts = [1,3]
+        let blueShirts = [4,5]
+        expect(tandemBicycle(redShirts, blueShirts, true)).toBe(9)
+    })
+
+    it('should return 32', ()=> {
+        let redShirts = [5, 5, 3, 9, 2]
+        let blueShirts = [3, 6, 7, 2, 1]
+        expect(tandemBicycle(redShirts, blueShirts, true)).toBe(32)
+    })
+
+})
+  
---
+++ print-elevation.js
@@ -0,0 +1,28 @@
+// Given an array of [1, 2, 3, 0, 2], print a map that
+// looks like
+/*
+      +
+    + +  +
+  + + +  + 
+*/
+
+
+const printElevation = (arr) => {
+    // What do I know?
+    // Input is an array of ints
+    // Highest point will need to print first
+    // 0 = ' ' (a space)
+
+    // Question everything?
+    
+    // DO I need to find the highest point?
+    // No, idea A eliminates the need for the highest point.
+
+    // 2 ideas
+        // A) Loop through array and create string of characters if the int is >= iteration #
+            // Loop through array for m where m = highest point
+        // B) Find the highest point, createa  2d array where inner array count is equal to highest point and translate the ints into the 2d array. Then print that array.
+            // Sort - O(nlogn) -- space lots of array space needed
+
+    
+}
