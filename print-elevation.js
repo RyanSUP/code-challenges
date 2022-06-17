b95dcde01e63dfbd579e70563183da592d323650
RMo <morici23@gmail.com>
2022-06-16 22:30:59 -0400

    Solve print-elevations part 1

--- print-elevation.js
+++ print-elevation.js
@@ -21,8 +21,44 @@ const printElevation = (arr) => {
     // 2 ideas
         // A) Loop through array and create string of characters if the int is >= iteration #
             // Loop through array for m where m = highest point
-        // B) Find the highest point, createa  2d array where inner array count is equal to highest point and translate the ints into the 2d array. Then print that array.
-            // Sort - O(nlogn) -- space lots of array space needed
+       // // B) Find the highest point, createa  2d array where inner array count is equal to highest point and translate the ints into the 2d array. Then print that array.
+       // //     Sort - O(nlogn) -- space lots of array space needed
 
-    
+    // Iterate through input array starting at 1.
+    // If i >= array[i] add a '+' to string
+    // Else add a space to string
+    // If a string is all spaces then I've reached the highest point and can break out of the loop.
+
+    // I Need...
+    // A string to store a row
+    // A string to store the entire elevation map
+    // A way to break out of the loop (highest point check)
+
+    let iteration = 1;
+    let searching = true;
+    let elevationStrings = []; // This will be used to print the final result
+
+    while(searching) {
+        let elevationString = '';
+        let foundMatch = false;
+        for(let i = 0; i < arr.length; i++) {
+            if(iteration <= arr[i]) {
+                elevationString += '+';
+                foundMatch = true;
+            } else {
+                elevationString += ' ';
+            }
+        }
+        if(foundMatch) {
+            elevationStrings.push(elevationString)
+        } else {
+            searching = false;
+        }
+        iteration++;
+    }
+    for(let i = elevationStrings.length - 1; i >= 0; i--) {
+        console.log(elevationStrings[i])
+    }
 }
+
+printElevation([1,2,3,0,1])
