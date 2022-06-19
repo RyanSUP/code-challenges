8f219ca2a1a2367ba4354b2ebb429d2043e6248c
RMo <morici23@gmail.com>
2022-06-19 13:24:16 -0400

    Solve tandem bicycle challenge

--- Tandem-Bicycle.test.js
+++ Tandem-Bicycle.test.js
@@ -1,6 +1,17 @@
 function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
 
+    redShirtSpeeds.sort((a,b) => a - b)
+    blueShirtSpeeds.sort((a,b) => a - b)
 
+    let totalSpeed = 0
+
+    if(fastest) redShirtSpeeds.reverse()
+
+    for(let i = 0; i < redShirtSpeeds.length; i++) {
+        totalSpeed += Math.max(redShirtSpeeds[i], blueShirtSpeeds[i])
+    }
+
+    return totalSpeed;
 }
   
 describe('Tandem-Bycycle', ()=> {
